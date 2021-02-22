b5b06f1c344ca109f56b1e344f7b5ddcac9be520
djonata-dias <diasdjonata@gmail.com>
2021-02-22 08:02:48 -0300

    adicionei o modulo body parser, tratei as rotas inexistentes e adicionei a rota /search

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
+const express = require('express');
+const { searchController } = require('./controller');
+const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.use('/search', searchController);
+app.get('*', (_req, res) => {
+  res.status(404).json({ message: 'Page not found' })
+});
 
 app.listen(PORT, () => {
-  console.log('servidor rodando');
-})
+  console.log('server running');
+});
