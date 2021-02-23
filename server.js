af7aa28e77e285feda43501257d423f3ae8b3628
djonata-dias <diasdjonata@gmail.com>
2021-02-23 20:30:02 -0300

    adicionei um redirecionamento da rota '/ ' para a página de pesquisa

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const { searchController } = require('./controller');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.get('/', (_res, req) => {
+  req.redirect('/search')
+});
 
 app.use('/search', searchController);
 
