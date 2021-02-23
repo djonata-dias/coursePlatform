811cd02d3122f22bef45c656f498b71f4c50c16d
djonata-dias <diasdjonata@gmail.com>
2021-02-23 12:59:03 -0300

    Ajustei o método post da rota para adicionar os videos em cada dia conforme o tempo que o usuário tem disponível

--- controller/searchController.js
+++ controller/searchController.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { google } = require('googleapis');
 const searchMock = require('../mock/searchMock.json');
 const videosListMock = require('../mock/videosListMock.json');
-
+const convertTime = require('../utils/convertTime');
 const router = express.Router();
 
 const searchGoogleApi = (params, video) => {
@@ -19,11 +19,11 @@ const searchGoogleApi = (params, video) => {
   return list;
 };
 
-router.get('/', (req, res) => {
-  res.status(200).render('index');
-});
+// router.get('/', (req, res) => {
+//   res.status(200).render('index');
+// });
 
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
   const { query, weekTime } = req.body;
   let params = {
     q: query,
@@ -31,28 +31,51 @@ router.post('/', async (req, res) => {
     maxResults: 50,
     type: 'video'
   };
-  const time = [15, 120, 30, 150, 20, 40, 90]
+  const timeList = [15, 120, 30, 150, 20, 40, 90]
   // Requisição que retorna a lista de filmes conforme 
   //const list = await searchList(params);
   const idList = [];
   const videosList = [];
-  const weekVideos = { monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: [] };
+  const weekVideos = [[], [], [], [], [], [], []];
 
   searchMock.data.items.forEach(({ id }) => {
-    idList.push(id.videoId)
+    idList.push(id.videoId);
   });
   params.part = ['snippet', 'contentDetails', 'player'];
   params.id = idList;
 
-  videosListMock.data.items.forEach{
-
+  videosListMock.data.items.forEach(({ snippet, tags, player, contentDetails }) => {
+    const video = {
+      title: snippet.title,
+      description: snippet.description,
+      duration: +convertTime(contentDetails.duration),
+      tags,
+      player
+    };
+    videosList.push(video);
+  })
+  let indexList = 0
+  let time = 0;
+  while (indexList < 7) {
+    for (let i = 0; i < videosList.length; i++) {
+      time += Number(videosList[i].duration);
+      if (Number(time) <= Number(timeList[indexList])) {
+        weekVideos[indexList].push(videosList[i]);
+        videosList.splice(i, 1);
+      } else {
+        break
+      }
+    }
+    indexList += 1;
+    time = 0;
   }
-
   // Requisição que retorna os dados dos filmes retornados na pesquisa 
   //const videos = await searchGoogleApi(params, true);
 
   // Realizei um mock na resposta da API pois as requisições são muito limitadas
-  res.status(200).json(videosListMock);
+  res.status(200).json(weekVideos);
 });
 
 module.exports = router;
+
+console.log(7.97 <= 30);
