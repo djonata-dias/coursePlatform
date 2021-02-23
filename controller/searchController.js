f4858054f2962c17cb4be2b9229c5491987f9704
djonata-dias <diasdjonata@gmail.com>
2021-02-23 11:10:42 -0300

    estou modelando o metodo post para retornar os vídeos para o front de acordo com cada dia da semana

--- controller/searchController.js
+++ controller/searchController.js
@@ -1,29 +1,58 @@
 require('dotenv').config();
 const express = require('express');
 const { google } = require('googleapis');
+const searchMock = require('../mock/searchMock.json');
+const videosListMock = require('../mock/videosListMock.json');
 
 const router = express.Router();
 
-const searchList = (params) => {
+const searchGoogleApi = (params, video) => {
   const youtube = google.youtube({
     version: 'v3',
     auth: process.env.API_KEY,
   });
+  if (video) {
+    const videos = youtube.videos.list(params)
+    return videos;
+  }
   const list = youtube.search.list(params)
   return list;
 };
 
-router.get('/:query', async (req, res) => {
-  data = [];
-  const params = {
-    q: req.params.query,
+router.get('/', (req, res) => {
+  res.status(200).render('index');
+});
+
+router.post('/', async (req, res) => {
+  const { query, weekTime } = req.body;
+  let params = {
+    q: query,
     part: ['id'],
     maxResults: 50,
     type: 'video'
   };
-  const videos = await searchList(params);
+  const time = [15, 120, 30, 150, 20, 40, 90]
+  // Requisição que retorna a lista de filmes conforme 
+  //const list = await searchList(params);
+  const idList = [];
+  const videosList = [];
+  const weekVideos = { monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: [] };
 
-  res.status(200).json(videos);
+  searchMock.data.items.forEach(({ id }) => {
+    idList.push(id.videoId)
+  });
+  params.part = ['snippet', 'contentDetails', 'player'];
+  params.id = idList;
+
+  videosListMock.data.items.forEach{
+
+  }
+
+  // Requisição que retorna os dados dos filmes retornados na pesquisa 
+  //const videos = await searchGoogleApi(params, true);
+
+  // Realizei um mock na resposta da API pois as requisições são muito limitadas
+  res.status(200).json(videosListMock);
 });
 
 module.exports = router;
